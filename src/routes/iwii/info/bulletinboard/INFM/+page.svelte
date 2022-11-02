2ff960477a63e72c9beca5ecb082f244a49d462d
Resaki1 <christian.reski@t-online.de>
2022-11-02 15:29:35 +0100

    improved news design

--- src/routes/iwii/info/bulletinboard/INFM/+page.svelte
+++ src/routes/iwii/info/bulletinboard/INFM/+page.svelte
@@ -11,7 +11,30 @@
 				<h2>{newsItem.title}</h2>
 				<h3>{newsItem.subTitle}</h3>
 				<p>{newsItem.content}</p>
+				<p>von {newsItem.nameOwner}</p>
 			</li>
 		{/each}
 	</ul>
 </section>
+
+<style lang="scss" scoped>
+	ul {
+		list-style: none;
+		padding: 0;
+		margin: 2rem;
+	}
+
+	li {
+		padding: 1rem;
+		background: #1e1e1e;
+		margin-bottom: 1rem;
+	}
+
+	h2 {
+		margin: 0;
+	}
+
+	h3 {
+		margin: 1rem 0;
+	}
+</style>
