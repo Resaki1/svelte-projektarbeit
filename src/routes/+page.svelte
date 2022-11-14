6d0bd382fc03c2c6a709297de41c576a7e239c13
Resaki1 <christian.reski@t-online.de>
2022-11-14 14:21:05 +0100

    added page redirect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,1 @@
-<h1>Willkommen bei SvelteKit</h1>
-<p>Besuche <a href="https://kit.svelte.dev">kit.svelte.dev</a> um die Doku zu lesen!</p>
-<a href="/iwii">Zur IWII-Seite</a>
+<p>Ups! Diese Seite existiert nicht</p>
