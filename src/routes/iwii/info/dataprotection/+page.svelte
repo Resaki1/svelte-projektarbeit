41d73f11b706d077ac79794062f6457c4480acae
Resaki1 <christian.reski@t-online.de>
2023-02-15 18:06:27 +0100

    added impressum

---
+++ src/routes/iwii/info/dataprotection/+page.svelte
@@ -0,0 +1,123 @@
+<div class="block">
+	<h2>Impressum</h2>
+	<p>
+		Die Hochschule Karlsruhe – Technik und Wirtschaft ist eine Körperschaft des Öffentlichen Rechts.
+		Sie wird durch den Rektor, Prof. Dr.-Ing. Frank Artinger, gesetzlich vertreten, Tel. <a
+			href="tel:+49721925-1001">+49 (0)7 21/9 25-10 01</a
+		> bzw. -10 02, Fax +49 (0)7 21/9 25-10 05.
+	</p>
+	<p>
+		Kontakt:
+		<br />
+		Hochschule Karlsruhe
+		<br />
+		Technik und Wirtschaft
+		<br />
+		University of Applied Sciences
+		<br />
+		Moltkestr. 30
+		<br />
+		76133 Karlsruhe
+	</p>
+	<p>
+		Postalisch:
+		<br />
+		Postfach 2440
+		<br />
+		76012 Karlsruhe
+	</p>
+	<p>
+		Tel. +49(0)721 925-0
+		<br />
+		Fax +49(0)721 925-2000
+		<br />
+		<a href="mailto:mailbox@h-ka.de">mailbox@h-ka.de</a>
+	</p>
+	<p>
+		Zuständige Aufsichtsbehörde: Ministerium für Wissenschaft, Forschung und Kunst
+		Baden-Württemberg, Stuttgart
+	</p>
+	<p>Umsatzsteuer-Identifikationsnummer gemäß § 27a Umsatzsteuergesetz: DE-14 35 89 180</p>
+	<p>
+		Texte, Bilder, Graphiken sowie das gesamte Layout wurden an der Hochschule Karlsruhe – Technik
+		und Wirtschaft bzw. in deren Auftrag erstellt und unterliegen dem Schutz des Urheberrechts. Sie
+		dürfen nur mit ausdrücklicher Genehmigung der Hochschule kopiert, verändert oder auf anderen
+		Internetseiten wieder gegeben werden. Allgemein zugängliche Texte dürfen von Dritten auch
+		auszugsweise nur dann genutzt werden, wenn die Hochschule Karlsruhe – Technik und Wirtschaft als
+		Urheber genannt wird. Nutzungsrechte für einzelne Elemente können auf Anfrage durch die
+		Hochschule erteilt werden.
+	</p>
+	<p>
+		Haftungsansprüche gegen die Hochschule, die sich auf Schäden materieller oder ideeller Art
+		beziehen, die aus der Nutzung bzw. Nichtnutzung der hochschuleigenen Internetseiten resultieren
+		oder aus der Nutzung fehlerhafter bzw. unvollständiger Informationen, schließen sich
+		grundsätzlich aus, sofern seitens der Hochschule kein nachweislich vorsätzliches oder grob
+		fahrlässiges Verschulden vorliegt.
+	</p>
+	<p>
+		Die Hochschule behält sich vor, Teile der Internetseiten oder das gesamte Angebot ohne
+		gesonderte Ankündigung zu verändern, zu ergänzen, zu löschen oder die Publikation zeitweise bzw.
+		endgültig einzustellen.
+		<br />
+		Haftungsausschluss: Sofern Teile oder einzelne Formulierungen des Impressums bzw. der eingefügten
+		Datenschutzerklärung der geltenden Rechtsvorschriften nicht, nicht mehr oder nur teilweise entsprechen,
+		bleiben die übrigen Teile dieser Dokumente in ihrem Inhalt und ihrer Gültigkeit davon unberührt.
+	</p>
+</div>
+<div class="block">
+	<h2>Hinweise zum Datenschutz</h2>
+	<p>Info-Seiten:</p>
+	<ul>
+		<li>
+			Der Betreiber dieser Info-Seiten ist die Fakultät für Informatik, Fachbereich Informatik (<a
+				href="mailto:informatik@h-ka.de">informatik@h-ka.de</a
+			>).
+		</li>
+		<li>
+			Die Seiten dienen ausschließlich der Wiedergabe von Informationen der Fakultät. Es werden
+			keine Daten erhoben. Möglicherweise auftretende Fehler werden ohne IP-Adresse des Aufrufers
+			gespeichert.
+		</li>
+		<li>
+			Wird eine andere Sprache als die im Browser voreingestellte gewählt, so wird diese im Cookie
+			lang gespeichert.
+		</li>
+		<li>
+			Vorgenommene Einstellungen wie die Art des Menüs, das Farbschema usw. werden in Cookies
+			gespeichert. Ohne Änderungen der Einstellungen werden keine Cookies verwendet.
+		</li>
+	</ul>
+	<p>Praxisbörse:</p>
+	<ul>
+		<li>
+			Der Betreiber der Praxisbörse ist die Fakultät für Informatik, Fachbereich Informatik (<a
+				href="mailto:informatik@h-ka.de">informatik@h-ka.de</a
+			>).
+		</li>
+		<li>Die Praxisbörse dient dazu, Firmen und Studierende in Kontakt zu bringen.</li>
+		<li>
+			Eine Anmeldung ist an dieser Stelle nur durch Firmen möglich. Dabei wird ein Sitzungs-Cookie
+			erzeugt, das nach Abmeldung oder einer Stunde Inaktivität gelöscht wird.
+		</li>
+		<li>
+			In der Praxisbörse werden nur solche Daten erhoben, die die Firmen selbst eintragen. Außer dem
+			Datum der letzten Anmeldung sowie dem Datum der Registrierung werden keine Nutzerstatistiken
+			erfasst.
+		</li>
+		<li>
+			Registrierte Ansprechpartner der Firmen können alle erfassten und automatisch erhobenen Daten
+			nach Anmeldung an der Praxisbörse unter Firmenprofil >> Datenschutz selbst herunterladen.
+		</li>
+	</ul>
+</div>
+
+<style lang="scss" scoped>
+	.block {
+		padding: 1rem;
+		background: #1e1e1e;
+		margin-bottom: 1rem;
+		box-shadow: 0 2px 1px -1px rgb(0 0 0 / 20%), 0 1px 1px 0 rgb(0 0 0 / 14%),
+			0 1px 3px 0 rgb(0 0 0 / 12%);
+		border-radius: 4px;
+	}
+</style>
