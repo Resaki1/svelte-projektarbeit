ed062bdaa769353201e6586205ab166b62197e87
Resaki1 <christian.reski@t-online.de>
2023-03-02 17:14:32 +0100

    tmp: removed redirect from editor

--- src/routes/iwii/faces/intranet/student/timetable/editor/+page.server.ts
+++ src/routes/iwii/faces/intranet/student/timetable/editor/+page.server.ts
@@ -1,13 +1,6 @@
 import { getAllTimetables } from '$lib/api';
-import { redirect } from '@sveltejs/kit';
 
-export const load = async ({ parent }: any) => {
-	const { user } = await parent();
-
-	if (!user.adsName) {
-		throw redirect(307, '/');
-	}
-
+export const load = async () => {
 	const allTimetables = await getAllTimetables(fetch);
 
 	return {
